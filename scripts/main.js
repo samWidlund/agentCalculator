a4bcbdbefbf1ddd7efa70b58e557084d8dc5aec6
samWidlund <sam.wid415@gmail.com>
2024-11-06 21:15:34 +0100

    add pivot calc

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,36 @@
 const calculateButton = document.getElementById("calculateButton");
-
-const resultLabel = document.getElementById("resultLabel");
-let result = 0;
+const ticketResultLabel = document.getElementById("ticketResultLabel");
 const ticketsPerHourLabel = document.getElementById("ticketsPerHour");
+const pivotResultLabel = document.getElementById("pivotResultLabel");
 
-resultLabel.textContent = "vendorCost-obFee:";
+let ticketsResult = 0;
+let pivotsResult = 0;
+
+// set default text
+ticketResultLabel.textContent = "vendorCost-obFee:";
 ticketsPerHourLabel.textContent = "ticketsPerHour:";
+pivotResultLabel.textContent = "pivotResult:";
 
 calculateButton.addEventListener("click", function() {
     const vendorCostInput = parseFloat(document.getElementById("vendorCost").value) || 0;
     const obFeeInput = parseFloat(document.getElementById("obFee").value) || 0;
+    ticketsResult = vendorCostInput - obFeeInput;
+    ticketResultLabel.textContent = ticketsResult;
+
     const ticketsDoneInput = parseFloat(document.getElementById("ticketsDone").value) || 0;
     const hoursWorkedInput = parseFloat(document.getElementById("hoursWorked").value) || 0;
-
-    result = vendorCostInput - obFeeInput;
-    resultLabel.textContent = result;
     ticketsPerHourLabel.textContent = ticketsDoneInput/hoursWorkedInput;
+
+    const pivotStartInput = parseFloat(document.getElementById("pivotStart").value) || 0;
+    const pivotEndInput = parseFloat(document.getElementById("pivotEnd").value) || 0;
+    pivotsResult = pivotEndInput - pivotStartInput;
+    pivotResultLabel.textContent = pivotsResult;
+    pivotResultLabel.textContent = pivotsResult;
 
     // debug
     console.log(ticketsDoneInput);
     console.log(vendorCostInput);
     console.log(obFeeInput);
-    console.log(result);
+    console.log(ticketsResult);
+    console.log(pivotsResult);
 })
